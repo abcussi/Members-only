902da4fe5d4f3ffadbde8ce1e9b571b1576e3853
abcussi <cussi.angel.benjamin@gmail.com>
2020-05-04 06:52:46 -0400

    generate a post model,the controller,view and routes with references to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_100950) do
+ActiveRecord::Schema.define(version: 2020_05_04_103724) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_05_04_100950) do
     t.string "remember_digest"
   end
 
+  add_foreign_key "posts", "users"
 end

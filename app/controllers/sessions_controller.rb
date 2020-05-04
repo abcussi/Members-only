c4ad4ae87a0a177126da2a005397b3ed23904785
abcussi <cussi.angel.benjamin@gmail.com>
2020-05-04 06:11:35 -0400

    add a session controller and change helpers methods invoke a remmeber digest controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,18 @@ class SessionsController < ApplicationController
     def new
     end
   
-    def login_user
-  
+    def create
+        user = User.find_by(email: params[:session][:email])
+        if user && user.authenticate(params[:session][:password])
+          log_in_user(user)
+          remember(user)
+          redirect_to feed_path
+        else
+    
+        end
+      end
+    
+      def delete
+        log_out(current_user)
     end
 end
